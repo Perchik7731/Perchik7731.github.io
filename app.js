4a76fd94671a0e05519c1ebdedaf028a66be9bb6
Perchik7731 <lekto773@gmail.com>
2024-06-17 12:13:45 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-let usercard = document.getElementById("usercard");
+let tg = window.Telegram.WebApp;
 
-let p = document.createElement("p");
+tg.expand();
 
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
 
-usercard.appendChild(p);
+let item = "";
 
+// Event listeners for each button
+let buttons = [btn1, btn2, btn3, btn4, btn5, btn6];
+buttons.forEach((btn, index) => {
+    btn.addEventListener("click", function(){
+        if (tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.setText(`Вы выбрали товар ${index + 1}!`);
+            item = `${index + 1}`;
+            tg.MainButton.show();
+        }
+    });
+});
 
+// Function to handle mainButtonClicked event
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    tg.sendData(item);
+});
 
-
-
-
-
-
+// Display user's name
+let username = document.getElementById("username");
+username.innerText = `Ваше имя: "${tg.initDataUnsafe.user.first_name}"`;
